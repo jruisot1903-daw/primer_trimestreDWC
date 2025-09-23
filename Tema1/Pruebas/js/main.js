aa4288d9f465baafb348dbf6a554e8cb967eca29
jruisot1903-daw <jruisot1903@g.educaand.es>
2025-09-23 09:51:40 +0200

    inicio

---
+++ Tema1/Pruebas/js/main.js
@@ -0,0 +1,11 @@
+// No es necesario meterlo en una funcion para que se ejecute
+/*function main (){
+    document.getElementById("h11").style.backgroundColor = "red";
+    document.getElementById("h11").style.color = "white";
+}
+main(); */
+
+    document.getElementById("h11").style.backgroundColor = "red";
+    document.getElementById("h11").style.color = "white";
+
+/************ FUNCTIONS ************/
