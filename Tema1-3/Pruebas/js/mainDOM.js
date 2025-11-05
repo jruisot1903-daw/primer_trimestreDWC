27f87f04f4dc5f72ba4f913ef775086be9dc4c8a
jruisot1903-daw <jruisot1903@g.educaand.es>
2025-11-05 10:25:36 +0100

    PruebasConNodos

--- Tema1-3/Pruebas/js/mainDOM.js
+++ Tema1-3/Pruebas/js/mainDOM.js
@@ -1,47 +1,89 @@
-const { createElement } = require("react");
 
 let info = document.getElementById("info");
 
-console.log(info.innerHTML);
+let pie = document.createElement("footer");
+let pPie = document.createElement("p");
+let pPieText = document.createTextNode("Contacto: 654 654 654");
 
+pPie.appendChild(pPieText);
+
+pie.appendChild(pPie);
+
+// document.body.appendChild(pie);
+document.body.innerHTML += "<label>Has ganado un coche. Pincha en este enlace: </label><a href='estafa.com'>Premio!!</a>"
+
+
+/***************************************************************** */
 document.getElementById("check").onclick = () => {
-  for (op of document.getElementsByName("opciones"))
-    if (op.checked) {
-      info.innerHTML = "Opción seleccionada: " + op.value;
-      break;
+    let op = document.getElementById("opcionLista").value;
+    let lista = null;
+    
+    if ((document.getElementsByTagName("ul").length > 0) && (op != "")) {
+        lista = document.getElementsByTagName("ul")[0];
+
+        lista.appendChild(createNode("li", op));
     }
-};
-
-// creacion de elementos HTML
-
-document.getElementById("check").onclick = () => {
-  //tenemos el texto que queremos insertar
-  let opc = document.getElementById("opcionLista").value;
-  //Nos devuelve un array con todas las listas ul del archivo en este caso queremos la primera solo
-  let lista = null;
-  if (document.getElementsByTagName("ul").length > 0 && opc != "") {
-    lista = document.getElementsByTagName("ul")[0];
-    //Creamos el elemento y el nodo del texto que queremos añadir
-    let opElement = document.createElement("li");
-    let opcElementText = document.createTextNode(opc);
-    //creamos el hijo al elemento
-    opElement.appendChild(opcElementText);
-
-    //a la lista le añadimos un hijo
-
-    lista.appendChild(opElement);
 
     document.getElementById("opcionLista").value = "";
-  } else {
-    alert("No has insertado nada");
-  }
+}
 
-  /**************************************************************/
-  function createNode(tipoNodo, tipoText) {
-    let nodo = createElement(tipoNodo);
-    let nodoText = createTextNode(tipoText);
+document.getElementById("bDelete").onclick = () => {
+    let lista = document.getElementById("lista");
+    let lastChild;
 
-    nodo.appendChild(nodoText);
+    if (lista.getElementsByTagName("li").length > 0) {
+       lastChild = lista.getElementsByTagName("li")[lista.getElementsByTagName("li").length-1];
+       let nodoBorrado = lastChild.parentNode.removeChild(lastChild);
+       console.log(nodoBorrado); // de esta forma nos devuleve el nodo borrado
+    }
+
+    // if (lista.getElementsByTagName("li").length > 0) {
+    //     lastChild = lista.getElementsByTagName("li")[lista.getElementsByTagName("li").length-1];
+    //     let nodoBorrado = lastChild.remove();
+    //     console.log(nodoBorrado); // de esta forma no podemos ver que nodo hemos borrado 
+    // }
+
+}
+
+/**************************************************************************/
+
+function createNode(tipoNodo, tipoTexto) {
+    let nodo;
+    let nodoText;
+
+    switch(arguments.length) {
+        case 0: 
+            throw "Se necesita al menos el tipo de elemento a crear.";
+            break;
+        case 1:
+            nodo = document.createElement(tipoNodo);
+            nodo.onclick = changeColor;
+            nodo.id = "nuevoNodo"
+            break;
+        case 2:
+            nodo = document.createElement(tipoNodo);
+            nodo.onclick = changeColor;
+            nodoText = document.createTextNode(tipoTexto);
+            nodo.appendChild(nodoText);
+            break;
+    }
 
     return nodo;
+}
+
+function changeColor() {
+    this.style.color = "red";
+}
+
+
+// Ver y ocultar contraseña
+document.getElementById("seePass").onclick = function () {
+    if (document.getElementById("pass").type == "password") {
+        document.getElementById("pass").type = "text"
+        this.innerHTML = "Ocultar password";
+    }
+    else {
+        document.getElementById("pass").type = "password"
+        this.innerHTML = "Ver password";
+    }
 }
